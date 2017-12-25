1c9194cb1958a9b5d697b2ac2fc89fe10ecbc7a4
xbc <1158839560@qq.com>
2017-12-25 20:59:48 +0800

    ...

--- src/components/fix/fix.vue
+++ src/components/fix/fix.vue
@@ -70,14 +70,15 @@
         }
       },
       getTop () {
-        let top = document.body.scrollTop
+        /* let top = document.body.scrollTop
         let timer = setInterval(() => {
           let speed = Math.ceil(top / 5)
           document.body.scrollTop = top - speed
         }, 20)
         if (top === 0) {
           clearInterval(timer)
-        }
+        } */
+        document.body.scrollTop = 0
       }
     },
     mounted () {
